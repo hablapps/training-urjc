a7f8850c8037c043c51fa36cdc9ec1061e977e5c
Jesús López-González <jesus@kathars.es>
2015-03-20 17:11:32 +0100

    Añadida versión reducida de la mónada Writer

--- scala/curso-urjc/tema4/functional/src/main/scala/es/urjc/etsii/ListaMonad.scala
+++ scala/curso-urjc/tema4/functional/src/main/scala/es/urjc/etsii/ListaMonad.scala
@@ -2,4 +2,16 @@ package es.urjc.etsii
 
 object ListaMonad extends App {
 
+  case class School(id: String, desc: Opcion[String])
+
+  case class Student(name: String, last: String, school: Opcion[School] = Ninguno)
+
+  val etsii = School(
+    "ETSII",
+    Algun("Escuela Técnica Superior de Ingeniería Informática"))
+
+  implicit val map: Map[String, Student] = Map(
+    "123456789A" -> Student("john", "smith"),
+    "987654321B" -> Student("peter", "parker", Algun(etsii)))
+
 }
